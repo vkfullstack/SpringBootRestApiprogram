a7e3e6d588860143bec338e2ff2d99d0b4ce669a
vkfullstack <vijaykhetre36@gmail.com>
2026-05-06 07:45:29 +0530

    create User entity with fields and JPA annotations

---
+++ src/main/java/in/vk/main/entity/User.java
@@ -0,0 +1,48 @@
+package in.vk.main.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private int id;
+	@Column
+	private String name;
+	@Column
+	private String email;
+	@Column
+	private String city;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+}
